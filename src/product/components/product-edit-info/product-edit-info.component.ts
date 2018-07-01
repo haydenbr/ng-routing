7c9cc3ad05e8da55c6a7831ff1048147042aecb1
Hayden Braxton <hayden.braxton.dev@gmail.com>
2018-07-01 13:43:25 -0400

    hook up child routes ... i dont think i like this whole child routing thing, but I'll learn it for the sake of the course

--- src/product/components/product-edit-info/product-edit-info.component.ts
+++ src/product/components/product-edit-info/product-edit-info.component.ts
@@ -2,16 +2,27 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 
+import { Subject } from 'rxjs';
+import { map, filter, takeUntil } from 'rxjs/operators';
+
 @Component({
-  templateUrl: 'product-edit-info.component.html'
+	templateUrl: 'product-edit-info.component.html'
 })
 export class ProductEditInfoComponent implements OnInit {
-  @ViewChild(NgForm) productForm: NgForm;
+	@ViewChild(NgForm) productForm: NgForm;
+	errorMessage: string;
+	product = { id: 1, productName: 'test', productCode: 'test' };
+	killSubs = new Subject();
 
-  errorMessage: string;
-  product = { id: 1, productName: 'test', productCode: 'test' };
+	constructor(private route: ActivatedRoute) {}
 
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit(): void {}
+	ngOnInit(): void {
+		this.route.parent.data
+			.pipe(
+				takeUntil(this.killSubs),
+				map((data) => data.product),
+				filter((p) => !!p)
+			)
+			.subscribe((p) => (this.product = p));
+	}
 }
--- src/product/components/product-edit-tags/product-edit-tags.component.ts
+++ src/product/components/product-edit-tags/product-edit-tags.component.ts
@@ -1,29 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Subject } from 'rxjs';
+import { map, filter, takeUntil } from 'rxjs/operators';
+
+import { Product } from '../../models';
+
 @Component({
-  templateUrl: 'product-edit-tags.component.html'
+	templateUrl: 'product-edit-tags.component.html'
 })
 export class ProductEditTagsComponent implements OnInit {
-  errorMessage: string;
-  newTags = '';
-  product = { id: 1, category: 'test', tags: ['test'] };
+	errorMessage: string;
+	newTags = '';
+	product: Product;
+	killSubs = new Subject();
 
-  constructor(private route: ActivatedRoute) {}
+	constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+	ngOnInit(): void {
+		this.route.parent.data
+			.pipe(
+				takeUntil(this.killSubs),
+				map((data) => data.product),
+				filter((p) => !!p)
+			)
+			.subscribe((p) => (this.product = p));
+	}
 
-  // Add the defined tags
-  addTags(): void {
-    const tagArray = this.newTags.split(',');
-    this.product.tags = this.product.tags
-      ? this.product.tags.concat(tagArray)
-      : tagArray;
-    this.newTags = '';
-  }
+	// Add the defined tags
+	addTags(): void {
+		const tagArray = this.newTags.split(',');
+		this.product.tags = this.product.tags ? this.product.tags.concat(tagArray) : tagArray;
+		this.newTags = '';
+	}
 
-  // Remove the tag from the array of tags.
-  removeTag(idx: number): void {
-    this.product.tags.splice(idx, 1);
-  }
+	// Remove the tag from the array of tags.
+	removeTag(idx: number): void {
+		this.product.tags.splice(idx, 1);
+	}
 }
